4a7afe2ff07cbdbafc5b302d6924195c7d0d8beb
maryamHabibpourr <Einesheikh_12@yahoo.com>
2024-03-29 22:34:06 +0330

    implement footer and header components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
+import "./globals.css";
+import { DM_Sans } from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
-import './globals.css'
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+});
 
 
 export default function RootLayout({
@@ -9,9 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-red-500">{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+    <html lang="en" className={dmSans.variable}>
+    <body className="grid bg-[#070815] text-white">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
     </html>
   );
 }
